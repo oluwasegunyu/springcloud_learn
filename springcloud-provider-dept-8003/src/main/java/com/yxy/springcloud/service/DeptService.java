4c6f829cf421afb2be9c723894273c46b2f034b9
yxy <charlay_yu@163.com>
2021-09-18 23:24:51 +0800

    ribbon 客户端负载均衡 多服务提供者

---
+++ springcloud-provider-dept-8003/src/main/java/com/yxy/springcloud/service/DeptService.java
@@ -0,0 +1,13 @@
+package com.yxy.springcloud.service;
+
+import com.yxy.springcloud.pojo.Dept;
+
+import java.util.List;
+
+public interface DeptService {
+    public boolean addDept(Dept dept);
+
+    public Dept queryById(Long id);
+
+    public List<Dept> queryAll();
+}
