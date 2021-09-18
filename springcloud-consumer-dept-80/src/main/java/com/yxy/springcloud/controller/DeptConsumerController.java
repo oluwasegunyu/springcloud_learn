8360c3116b6621918d7c749bfbe8146898a721ac
yxy <charlay_yu@163.com>
2021-09-18 22:27:17 +0800

    ribbon 客户端负载均衡 单服务提供者

--- springcloud-consumer-dept-80/src/main/java/com/yxy/springcloud/controller/DeptConsumerController.java
+++ springcloud-consumer-dept-80/src/main/java/com/yxy/springcloud/controller/DeptConsumerController.java
@@ -16,7 +16,8 @@ public class DeptConsumerController {
     @Autowired
     private RestTemplate restTemplate;
 
-    private static final String REST_URL_PREFIX = "http://localhost:8001";
+//    private static final String REST_URL_PREFIX = "http://localhost:8001";
+    private static final String REST_URL_PREFIX = "http://SPRINGCLOUD-PROVIDER-DEPT";
 
     @GetMapping("/consumer/dept/get/{id}")
     public Dept get(@PathVariable("id") Long id){
