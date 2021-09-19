d1d962b602a5fcf199153fff252f5aa92e360ae8
yxy <charlay_yu@163.com>
2021-09-19 18:05:45 +0800

    zuul 路由网关

---
+++ springcloud-zuul-9527/src/main/java/com/yxy/springcloud/ZuulApplication_7527.java
@@ -0,0 +1,13 @@
+package com.yxy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication_7527 {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication_7527.class, args);
+    }
+}
