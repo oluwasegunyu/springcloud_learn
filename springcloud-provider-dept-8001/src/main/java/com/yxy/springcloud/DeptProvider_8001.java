17869d51297d8a25837cf4d247a2da294db7e590
yxy <charlay_yu@163.com>
2021-09-19 17:18:22 +0800

    Hystrix 服务监控

--- springcloud-provider-dept-8001/src/main/java/com/yxy/springcloud/DeptProvider_8001.java
+++ springcloud-provider-dept-8001/src/main/java/com/yxy/springcloud/DeptProvider_8001.java
@@ -1,8 +1,11 @@
 package com.yxy.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,4 +13,13 @@ public class DeptProvider_8001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_8001.class, args);
     }
+
+    @Bean
+    public ServletRegistrationBean hy(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        return registrationBean;
+    }
+
 }
+
--- springcloud-provider-dept-hystrix-8001/src/main/java/com/yxy/springcloud/DeptProviderHystrix_8001.java
+++ springcloud-provider-dept-hystrix-8001/src/main/java/com/yxy/springcloud/DeptProviderHystrix_8001.java
@@ -1,10 +1,13 @@
 package com.yxy.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,5 +15,12 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 public class DeptProviderHystrix_8001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProviderHystrix_8001.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean hy(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        return registrationBean;
     }
 }
